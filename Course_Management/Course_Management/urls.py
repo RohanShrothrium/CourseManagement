410972e74262cbcb32fc5f331e1fecb6bc6eaf62
rohanshrothrium <shrothriumrohan@gmail.com>
2019-11-15 22:19:41 +0530

    nothing too big

--- Course_Management/Course_Management/urls.py
+++ Course_Management/Course_Management/urls.py
@@ -24,6 +24,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('profile/', user_views.profile, name='profile'),
+    path('courses/', user_views.courses, name='courses'),
+    path('projects/', user_views.projects, name='projects'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('portal.urls'))
 ]
--- Course_Management/users/views.py
+++ Course_Management/users/views.py
@@ -7,6 +7,22 @@ from django.contrib.auth.models import User
 @login_required
 def profile(request):
     context = {
-        'projects': request.user.takesproject_set.all()
+        'projects': request.user.takesproject_set.all(),
+        'courses': request.user.takescourse_set.all()
     }
     return render(request, 'users/profile.html', context)
+
+
+def courses(request):
+    context = {
+        'courses': request.user.takescourse_set.all()
+    }
+    return render(request, 'users/courses.html', context)
+
+
+def projects(request):
+    context = {
+        'projects': request.user.takesproject_set.all(),
+        'courses': request.user.takescourse_set.all()
+    }
+    return render(request, 'users/projects.html', context)
